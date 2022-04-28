3a0a5054c819073fcd48a891bcc1b32466f2268a
disha89 <disha89ne@gmail.com>
2022-04-28 17:06:28 +0700

    if else switch conditions part one

--- js/script.js
+++ js/script.js
@@ -18,3 +18,35 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+const number = 50;
+
+if(number == 10) {
+    console.log('Not Good');
+} else if (number === 10) {
+    console.log('Very Not Good');
+} else if (number == 10) {
+    console.log('Very Very Not Good');
+} else {
+    console.log('Akuna Matata');
+}
+
+(number === 50) ? console.log('Hola') : console.log('Nihao');
+
+const num = 30;
+
+switch (num) {
+    case 50:
+        console.log('Work');
+        break;
+    case 40:
+        console.log('Work Toy');
+        break;
+    case 70:
+        console.log('Tros');
+        break;
+    default:
+        console.log('Pumba');
+        break;
+}
+
