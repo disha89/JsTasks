1e695fc0b0d1499d78da57368c9bca5a4d624b58
disha89 <disha89ne@gmail.com>
2022-04-30 16:25:31 +0700

    if else switch conditions part last

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 const numberOfFilms = +prompt('Сколько Фильмов Вы Уже Посмотрели?', '');
 
 const personalMovieDB = {
@@ -21,7 +20,7 @@ console.log(personalMovieDB);
 
 const number = 50;
 
-if(number == 10) {
+if (number == 10) {
     console.log('Not Good');
 } else if (number === 10) {
     console.log('Very Not Good');
@@ -31,7 +30,7 @@ if(number == 10) {
     console.log('Akuna Matata');
 }
 
-(number === 50) ? console.log('Hola') : console.log('Nihao');
+(number === 50) ? console.log('Hola'): console.log('Nihao');
 
 const num = 30;
 
@@ -50,3 +49,58 @@ switch (num) {
         break;
 }
 
+
+const hamburger = true;
+const fries = true;
+
+if (hamburger && fries) {
+    console.log('Vse Est');
+}
+
+
+const chisburger = 31;
+const hotDog = 1111;
+
+if (chisburger === 3 && hotDog) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+const vodka = 0;
+const viski = 3;
+const brendi = 5;
+
+if (vodka === 2 || viski || brendi) {
+    console.log('Vse Good');
+} else {
+    console.log('Idem Domoy');
+}
+
+console.log((vodka === 2 || viski || brendi));
+
+
+console.log(NaN || 2 || undefined);
+
+console.log(NaN && 2 && undefined);
+
+console.log(1 && 2 && 3);
+
+console.log(!1 && 2 || !3);
+
+console.log(25 || null && !3);
+
+console.log(NaN || null || !3 || undefined || 5);
+
+console.log(NaN || null && !3 && undefined || 5);
+
+console.log(5 === 5 && 3 > 1 || 5);
+
+let hamburgerr;
+const friess = NaN;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburgerr && cola || friess === 3 && nuggets) {
+    console.log('Done!');
+}
